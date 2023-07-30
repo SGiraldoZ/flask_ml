c299ce6474c4ad283247a3e412041963886295c6
Sebas <sebasgizu@gmail.com>
2023-07-30 14:19:10 -0500

    Add a quick prediction with the imported model

--- app/app.py
+++ app/app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from joblib import load
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def base_route():
+    test_np = np.array([[1],[2]])
+    model = load('../model/model.joblib')
+    preds = model.predict(test_np)
+    return str(preds)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
+
+    
